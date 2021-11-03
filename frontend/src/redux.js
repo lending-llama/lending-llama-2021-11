df317595d1d8f4b8ad834ff21ea3ba58bf10de9e
Mia Krause <mkrause@sipgate.de>
2021-11-03 11:55:30 +0100

    tests: Decouple test from each other by creating a new redux store

--- frontend/src/redux.js
+++ frontend/src/redux.js
@@ -3,11 +3,11 @@ import {errorsReducer} from "./reducers/errors";
 import {featuresReducer} from "./reducers/features";
 import {allocationsReducer} from "./reducers/allocations";
 
-export const store = createStore(
-  combineReducers({
+export const createOwnStore = () => {return createStore( combineReducers({
     allocations: allocationsReducer,
     features: featuresReducer,
     errors: errorsReducer,
   }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}
+
+export const store = createOwnStore()
