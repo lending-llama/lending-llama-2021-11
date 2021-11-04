bd236dc26f9a6c36f617089fd9118c7b5809083f
Alex Bepple <alex@bepple.de>
2021-11-04 13:23:17 +0100

    ! harmonize more & improve name

--- frontend/src/fetchJsonFromBackend.js
+++ frontend/src/fetchJsonFromBackend.js
@@ -1,7 +1,7 @@
 import {errorsAdded} from "./actions/errors";
 
 export function fetchJsonFromBackend(url, dispatch) {
-  return fetch(url)
+  return fetch("/api"+url)
   .then(async x => {
     if (x.status >= 400) {
       throw new Error(await x.text())
